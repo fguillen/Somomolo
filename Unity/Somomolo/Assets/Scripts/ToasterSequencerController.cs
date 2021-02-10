bedfad251f0fd6a3803fd2a04766888017f15a91
Fernando Guillen <fguillen.mail@gmail.com>
2021-02-10 21:28:37 +0100

    Toasters start spawning toasts

---
+++ Unity/Somomolo/Assets/Scripts/ToasterSequencerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ToasterSequencerController : MonoBehaviour
+{
+    [SerializeField] ToastController[] toasts;
+    [SerializeField] ToasterController[] toasters;
+    List<ToastController[]> sequences;
+
+    void Awake()
+    {
+        sequences = new List<ToastController[]>();    
+    }
+
+    void Start()
+    {
+        SpawnNewSequence();
+    }
+
+    public ToastController[] CreateNewSequence()
+    {
+        ToastController[] sequence = new ArraySegment<ToastController>(toasts, sequences.Count, 3).Array;
+        sequences.Add(sequence);
+
+        return sequence;
+    }
+
+    public void SpawnNewSequence()
+    {
+        var sequence = CreateNewSequence();
+
+        for (int i = 0; i < toasters.Length; i++)
+        {
+            var toastPrefab = sequence[i].gameObject;
+            var toaster = toasters[i];
+
+            // GameObject toast = Instantiate(toastPrefab, Vector3.zero, Quaternion.identity, toaster.slot.transform, false);
+            GameObject toast = Instantiate(toastPrefab, toaster.slot.transform);
+            // toast.transform.parent = toaster.slot.transform;
+        }
+    }
+
+}
