c86d31a5b1b61d3083af3e82bddfdf6898b92eb6
Fernando Guillen <fguillen.mail@gmail.com>
2021-02-14 21:16:25 +0100

    Chicks are jumping around

---
+++ Unity/Somomolo/Assets/Scripts/ChickenController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ChickenController : MonoBehaviour
+{
+    [SerializeField] Transform[] jumpingPoints;
+    Transform actualJumpingPoint;
+
+    [SerializeField] LinearProportionConverter jumpingHighCalculator;
+    
+
+    string state;
+
+    void Awake()
+    {
+        state = "idle";
+    }
+
+    void Start()
+    {
+        GoToRandomJumpingPoint();
+    }
+
+    void Update()
+    {
+        if(state == "idle")
+        {
+            if(Random.Range(0, 100) == 0)
+            {
+                GoToRandomJumpingPoint();
+            }
+        }    
+    }
+
+    Transform RandomJumpingPoint()
+    {
+        return jumpingPoints[Random.Range(0, jumpingPoints.Length)];
+    }
+
+    void GoToRandomJumpingPoint()
+    {        
+        actualJumpingPoint = RandomJumpingPoint();
+        state = "jumping";
+
+        StartCoroutine("JumpCorroutine");
+    }
+
+    IEnumerator JumpCorroutine()
+    {
+        float count = 0.0f;
+
+        Vector3 actualPosition = transform.position;
+        Vector3 jumpingPointPosition = actualJumpingPoint.position;
+        Vector3 controlPosition = CalculateControlPoint(actualPosition, jumpingPointPosition);
+        
+
+        while (count < 1.0f) {
+            count += 1.0f * Time.deltaTime;
+
+            Vector3 m1 = Vector3.Lerp( actualPosition, controlPosition, count );
+            Vector3 m2 = Vector3.Lerp( controlPosition, jumpingPointPosition, count );
+            transform.position = Vector3.Lerp(m1, m2, count);
+
+            yield return null;
+        }
+
+        state = "idle";
+    }
+
+    Vector3 CalculateControlPoint(Vector3 point1, Vector3 point2)
+    {
+        Vector3[] points = {point1, point2};
+        var pointMinY = points.OrderBy(e => e.y).ToArray()[0];
+        var pointMaxY = points.OrderBy(e => e.y).ToArray()[1];
+        var pointMinX = points.OrderBy(e => e.x).ToArray()[0];
+        var pointMaxX = points.OrderBy(e => e.x).ToArray()[1];
+
+        jumpingHighCalculator.dimension2Max = (Mathf.Abs(pointMaxY.x - pointMinY.x) / 2.0f);
+
+        var controlPointY = jumpingHighCalculator.CalculateDimension2Value(pointMaxY.y - pointMinY.y) + pointMaxY.y;
+        var controlPointX = ((pointMaxX.x - pointMinX.x)/ 2.0f) + pointMinX.x;
+
+        var controlPoint = new Vector3(controlPointX, controlPointY, 0);
+
+        return controlPoint;
+    }
+}
---
+++ Unity/Somomolo/Assets/Scripts/LinearProportionConverter.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System;
+
+[Serializable] public class LinearProportionConverter
+{
+    [SerializeField] public float dimension1Min;
+    [SerializeField] public float dimension1Max;
+    [SerializeField] public float dimension2Min;
+    [SerializeField] public float dimension2Max;
+
+    public LinearProportionConverter(float dimension1Min, float dimension1Max, float dimension2Min, float dimension2Max)
+    {
+        this.dimension1Min = dimension1Min;
+        this.dimension1Max = dimension1Max;
+        this.dimension2Min = dimension2Min;
+        this.dimension2Max = dimension2Max;
+    }
+
+    public float CalculateDimension1Value(float dimension2Value)
+    {
+        var a = dimension2Min;
+        var b = dimension2Max;
+        var c = dimension1Min;
+        var d = dimension1Max;
+        var e = dimension2Value;
+
+        var result = c + (((e - a) * (d - c)) / (b - a));
+
+        return result;
+    }
+
+    public float CalculateDimension2Value(float dimension1Value)
+    {
+        var a = dimension1Min;
+        var b = dimension1Max;
+        var c = dimension2Min;
+        var d = dimension2Max;
+        var e = dimension1Value;
+
+        Debug.Log("dimension1Min: " + dimension1Min);
+        Debug.Log("dimension1Max: " + dimension1Max);
+        Debug.Log("dimension2Min: " + dimension2Min);
+        Debug.Log("dimension2Max: " + dimension2Max);
+        Debug.Log("dimension1Value: " + dimension1Value);
+
+        var result = c + (((e - a) * (d - c)) / (b - a));
+
+        return result;
+    }
+}
